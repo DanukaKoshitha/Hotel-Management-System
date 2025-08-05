81d0575cf43015f61a5adb9ad8d2be81b36c78eb
Danuka <danukakoshitha2004@gmail.com>
2025-08-05 22:35:48 +0530

    First Commit

---
+++ Hotel-Managment-Service-API/Hotel-Managment-Service-API/src/main/java/com/hotel_management_system/Hotel_Managment_Service_API/HotelManagmentServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_management_system.Hotel_Managment_Service_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagmentServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagmentServiceApiApplication.class, args);
+	}
+
+}
---
+++ Hotel-Managment-Service-API/Hotel-Managment-Service-API/src/test/java/com/hotel_management_system/Hotel_Managment_Service_API/HotelManagmentServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_management_system.Hotel_Managment_Service_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagmentServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
