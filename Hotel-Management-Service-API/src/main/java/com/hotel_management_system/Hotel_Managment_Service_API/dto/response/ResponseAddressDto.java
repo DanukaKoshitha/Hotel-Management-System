b27a7bfd827a1aa2781991a19728fee0c49743ab
Danuka <danukakoshitha2004@gmail.com>
2025-08-09 14:32:45 +0530

    Create Response Address dto

--- Hotel-Management-Service-API/src/main/java/com/hotel_management_system/Hotel_Managment_Service_API/dto/response/ResponseAddressDto.java
+++ Hotel-Management-Service-API/src/main/java/com/hotel_management_system/Hotel_Managment_Service_API/dto/response/ResponseAddressDto.java
@@ -1,4 +1,20 @@
 package com.hotel_management_system.Hotel_Managment_Service_API.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseAddressDto {
+    private String addressLine;
+    private String city;
+    private String country;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String branchId;
 }
