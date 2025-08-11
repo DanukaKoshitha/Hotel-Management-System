4012bceee593536e968e2c9ed18c16fa423d961f
Danuka <danukakoshitha2004@gmail.com>
2025-08-11 22:58:12 +0530

    Create Branch Entity

---
+++ Hotel-Management-Service-API/src/main/java/com/hotel_management_system/Hotel_Managment_Service_API/entity/BranchEntity.java
@@ -0,0 +1,40 @@
+package com.hotel_management_system.Hotel_Managment_Service_API.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "branch")
+public class BranchEntity {
+    @Id
+    @Column(name = "branch_id" , length = 80)
+    private String branchId;
+
+    @Column(name = "branch_name" , nullable = false)
+    private String branchName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "branch_type" , nullable = false)
+    private BranchEntity branchEntity;
+
+    @Column(name = "room_count")
+    private int roomCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_id")
+    private String hotel;
+
+    @OneToOne(mappedBy = "branch")
+    private AddressEntity address;
+
+//    @OneToMany(mappedBy = "branch")
+//    private List<RoomEntity> room;
+}
