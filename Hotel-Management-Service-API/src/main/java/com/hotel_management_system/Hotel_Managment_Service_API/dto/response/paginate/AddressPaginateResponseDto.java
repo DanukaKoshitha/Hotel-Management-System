75c6a908fe86b36c977e22cfeabd7024f7abf11d
Danuka <danukakoshitha2004@gmail.com>
2025-08-15 11:25:26 +0530

    Create Address Paginate ResponseDto

---
+++ Hotel-Management-Service-API/src/main/java/com/hotel_management_system/Hotel_Managment_Service_API/dto/response/paginate/AddressPaginateResponseDto.java
@@ -0,0 +1,17 @@
+package com.hotel_management_system.Hotel_Managment_Service_API.dto.response.paginate;
+
+import com.hotel_management_system.Hotel_Managment_Service_API.dto.response.ResponseAddressDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressPaginateResponseDto {
+    private List<ResponseAddressDto> dataList;
+    private long dataCount;
+}
