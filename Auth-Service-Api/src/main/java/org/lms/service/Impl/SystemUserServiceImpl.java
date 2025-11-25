6b1ea4974ac8554bad5f0589401c41af3303d68e
Danuka <danukakoshitha2004@gmail.com>
2025-11-25 12:31:00 +0530

    implemented security authentication.

--- Auth-Service-Api/src/main/java/org/lms/service/Impl/SystemUserServiceImpl.java
+++ Auth-Service-Api/src/main/java/org/lms/service/Impl/SystemUserServiceImpl.java
@@ -31,7 +31,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
 import java.io.IOException;
 import java.time.Instant;
 import java.util.*;
